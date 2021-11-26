a20d29a68c593900df92d4e1b183bb7ded9ecbb3
abs3nce <vodrazka.jakub@gmail.com>
2021-11-26 11:10:54 +0100

    + password forgot and password reset handlers

--- controllers/controller_account.js
+++ controllers/controller_account.js
@@ -18,7 +18,7 @@ exports.registerUser = async (req, res, next) => {
     const user = await new User(req.body); //pokial tento user neexistoval >> vytvor novy instance Usera s udajmi z FE
     await user.save(); //uloz ho do DB a na FE posli response s udajmi
 
-    console.log(`API > SAVING USER TO DB: ${user}`);
+    console.log(`API (REGISTER) > SAVING USER TO DB: ${user}`);
     res.status(200).json({
         user: {
             username: user.username,
@@ -58,7 +58,9 @@ exports.loginUser = (req, res, next) => {
         res.cookie("token", token, { expire: new Date() + 3600 });
 
         //return usera s tokenom na FE
-        console.log(`API > USER ${user.username} LOGGED IN:`, user, { token });
+        console.log(`API (LOGIN) > USER ${user.username} LOGGED IN:`, user, {
+            token,
+        });
         return res.status(200).json({
             token: token,
             user: { username: user.username, email: user.email, _id: user._id },
@@ -79,3 +81,122 @@ exports.requireLogin = expressJWT({
     algorithms: ["HS256"], // definovane kvoli novej verzii express-jwt
     //pokial je token validny >> expressJWT prida idcko overeneho usera do request objectu vo forme auth klucu
 });
+
+exports.forgotPassword = (req, res) => {
+    if (!req.body)
+        return res.status(400).json({ message: "No request body found" });
+    if (!req.body.email)
+        return res
+            .status(400)
+            .json({ message: "No email found in request body" });
+
+    console.log(
+        `API (FORGOT PASSWORD) > FINDING USER WITH EMAIL "${req.body.email}"`
+    );
+    const { email } = req.body;
+
+    User.findOne({ email: email }, (err, user) => {
+        if (err || !user) {
+            console.log(`API (FORGOT PASSWORD) > USER NOT FOUND`);
+            console.log(`API (FORGOT PASSWORD) > RETURNING WITH ERROR`);
+            return res
+                .status(500)
+                .json({ error: "User with that email does not exist" });
+        }
+
+        console.log(
+            `API (FORGOT PASSWORD) > USER FOUND: ${user.username} (${user})`
+        );
+
+        console.log(
+            `API (FORGOT PASSWORD) > GENERATING NEW TOKEN FOR USER ${user.username}`
+        );
+        //vygenerovanie tokenu
+        const token = jwt.sign(
+            {
+                _id: user._id,
+                issuer: "NODEAPI",
+            },
+            process.env.JWT_SECRET
+        );
+        console.log(
+            `API (FORGOT PASSWORD) > FINISHED GENERATING NEW TOKEN FOR USER ${user.username}`
+        );
+
+        console.log(`API (FORGOT PASSWORD) > CREATING NEW EMAIL MESSAGE`);
+        //email data
+        const emailData = {
+            from: "noreply@socnet-app.com",
+            to: email,
+            subject: "Password Reset Instrucions",
+            text: `Please use the following link to reset you password: ${process.env.CLIENT_RESET_PASSWORD_URL}/reset-password/${token}`,
+            html: `<p>Please use the following link to reset you password: </p><p>${process.env.CLIENT_RESET_PASSWORD_URL}/reset-password/${token}</p>`,
+        };
+        console.log(
+            `API (FORGOT PASSWORD) > FINISHED CREATING NEW EMAIL MESSAGE`
+        );
+
+        console.log(
+            `API (FORGOT PASSWORD) > SENDING EMAIL TO USER ${user.username} ON ${user.email} `
+        );
+        return user.updateOne({ resetPasswordLink: token }, (err, success) => {
+            if (err) {
+                return res.status(500).json({ message: err });
+            } else {
+                sendEmail(emailData);
+                console.log(
+                    `API (FORGOT PASSWORD) > EMAIL HAS BEEN SUCCESSFULLY SENT TO USER ${user.username} ON ${user.email} `
+                );
+                return res.status(200).json({
+                    message: `Email has been successfully sent to ${user.email}. Follow the instructions appended in the email to reset your password`,
+                });
+            }
+        });
+    });
+};
+
+/*
+Na to aby si uzivatel vedel zmenit heslo
+potrebujem najprv najst v DB uzivatela s jeho "resetPasswordLink"-om
+hodnota tohoto resetPasswordLinku sa musi zhodovat s uzivatelskym tokenom
+pokial sa uzivatelsky resetPasswordLink(token) zhoduje s prichadzajucim req.body.resetPasswordLink(token) tak mame spravneho uzivatela
+>> nasledne mu vieme zmenit heslo
+*/
+
+exports.resetPassword = (req, res) => {
+    const { resetPasswordLink, newPassword } = req.body;
+
+    User.findOne({ resetPasswordLink: resetPasswordLink }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "Invalid Link",
+            });
+        }
+
+        console.log(
+            `API (RESET PASSWORD) > UPDATING USER (${user.username}) WITH NEW DATA`
+        );
+        const updatedFields = {
+            password: newPassword,
+            resetPasswordLink: "",
+        };
+
+        user = _.extend(user, updatedFields);
+        user.updated = Date.now();
+        console.log(
+            `API (RESET PASSWORD) > USER ${user.username} UPDATED ON ${user.updated}`
+        );
+
+        console.log(`API (RESET PASSWORD) > SAVING USER ${user.username}`);
+
+        user.save((err, result) => {
+            if (err) return res.status(500).json({ error: err });
+            console.log(
+                `API (RESET PASSWORD) > USER ${user.username} SUCCESSFULLY SAVED`
+            );
+            res.status(200).json({
+                message: "Great! Now you can login with your new password",
+            });
+        });
+    });
+};
